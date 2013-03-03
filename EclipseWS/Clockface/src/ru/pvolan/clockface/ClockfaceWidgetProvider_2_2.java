2e4b33b4d215a4be4e034524e538bd5f12efa6c4
unknown <PVoLan@PVoLan_Asus.(none)>
2013-03-03 18:47:23 +0400

    Renamed current widget to clockface_81

--- EclipseWS/Clockface/src/ru/pvolan/clockface/ClockfaceWidgetProvider_2_2.java
+++
@@ -1,5 +0,0 @@
-package ru.pvolan.clockface;
-
-public class ClockfaceWidgetProvider_2_2 extends ClockfaceWidgetProvider {
-
-}
---
+++ EclipseWS/Clockface/src/ru/pvolan/clockface/ClockfaceWidgetProvider_81_2_2.java
@@ -0,0 +1,5 @@
+package ru.pvolan.clockface;
+
+public class ClockfaceWidgetProvider_81_2_2 extends ClockfaceWidgetProvider {
+
+}
