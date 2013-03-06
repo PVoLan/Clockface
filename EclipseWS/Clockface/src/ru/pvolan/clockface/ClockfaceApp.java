5c8dcbeeb4c0c8a15c86f8cda0e26ced1f3968a4
PVoLan <pvolan@mail.ru>
2013-03-06 16:58:59 +0400

    Attempt to add GA Exception tracking (untested).

--- EclipseWS/Clockface/src/ru/pvolan/clockface/ClockfaceApp.java
+++ EclipseWS/Clockface/src/ru/pvolan/clockface/ClockfaceApp.java
@@ -2,6 +2,10 @@ package ru.pvolan.clockface;
 
 import java.lang.Thread.UncaughtExceptionHandler;
 
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.ExceptionReporter;
+import com.google.analytics.tracking.android.GAServiceManager;
+
 import ru.pvolan.trace.Trace;
 
 import android.app.Application;
@@ -21,17 +25,32 @@ public class ClockfaceApp extends Application
 		super.onCreate();
 		Trace.Print("ClockfaceApp.onCreate");
 		
-		Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+		
+		
+		final UncaughtExceptionHandler systemHandler = Thread.getDefaultUncaughtExceptionHandler(); 
+		
+		final UncaughtExceptionHandler logHandler = new Thread.UncaughtExceptionHandler() {
 			
 			@Override
 			public void uncaughtException(Thread thread, Throwable ex) {
 				// TODO Auto-generated method stub
 				Trace.Print("*******Error******");
 				Trace.Print(ex);
+				systemHandler.uncaughtException(thread, ex);
 			}
-		});
-			
+		};
 		
+
+		
+		EasyTracker.getInstance().setContext(getApplicationContext());
+			
+		UncaughtExceptionHandler gaHandler = new ExceptionReporter(
+			    EasyTracker.getTracker(),
+			    GAServiceManager.getInstance(),
+			    logHandler);
+
+		 // Make myHandler the new default uncaught exception handler.
+		Thread.setDefaultUncaughtExceptionHandler(gaHandler);
 	}
 	
 }
--- EclipseWS/Clockface/src/ru/pvolan/clockface/SetupActivity.java
+++ EclipseWS/Clockface/src/ru/pvolan/clockface/SetupActivity.java
@@ -1,8 +1,13 @@
 package ru.pvolan.clockface;
 
+import com.google.analytics.tracking.android.GAServiceManager;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SetupActivity extends Activity {
 
@@ -10,6 +15,20 @@ public class SetupActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        
+        //GAServiceManager.getInstance().
+        GAServiceManager.getInstance().dispatch();
+        
+        
+        Button b = (Button)findViewById(R.id.buttonCrash);
+        b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				throw new RuntimeException("My own one");				
+			}
+		});
     }
 
 
