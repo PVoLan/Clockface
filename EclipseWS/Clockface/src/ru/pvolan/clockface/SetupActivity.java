f8ec8480dcd196ce4a5db3fd05ff1138d811ad98
PVoLan <pvolan@mail.ru>
2014-11-13 17:09:43 +0300

    Remove unused things

--- EclipseWS/Clockface/src/ru/pvolan/clockface/SetupActivity.java
+++ EclipseWS/Clockface/src/ru/pvolan/clockface/SetupActivity.java
@@ -32,11 +32,4 @@ public class SetupActivity extends Activity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.setup, menu);
-        return true;
-    }
-    
 }
